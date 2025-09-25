fa0280ddf1fea755b13a7e6ffb745a0d12db36af
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-09-25 13:55:01 -0400

    just testing stuff

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	tokenStream := Lexer.GenerateTokenStream("./fib.ion")
+	tokenStream := Lexer.GenerateTokenStream("./test.ion")
 	for i := 0; i < len(tokenStream); i++ {
 		token := tokenStream[i]
 		tokenType, tokenValue := token.Kind, token.Lexeme
